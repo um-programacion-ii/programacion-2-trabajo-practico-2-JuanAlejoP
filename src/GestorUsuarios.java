ca24949a9d299d7a0f44531c3e3c625c378a0b1a
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-04-21 22:21:12 -0300

    feat: Implementar clase base GestorUsuarios

---
+++ src/GestorUsuarios.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
+
+
+public class GestorUsuarios {
+    private Map<String, Usuario> usuarios;
+
+    public GestorUsuarios() {
+        this.usuarios = new HashMap<>();
+    }
+
+    public void addNewUser(Usuario usuario) {
+        String id = usuario.getId();
+        usuarios.put(id, usuario);
+    }
+
+    public Usuario searchUser(String id) {
+        Usuario usuario = usuarios.get(id);
+        return usuario;
+    }
+
+    public Collection<Usuario> listAllUsers() {
+        return usuarios.values();
+    }
+}
