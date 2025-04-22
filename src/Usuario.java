598e744971bffb5ccb77c266c932776942779582
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-04-21 22:00:00 -0300

    feat: Implementar clases base Usuario, RecursoDigital, EstadoRecurso

---
+++ src/Usuario.java
@@ -0,0 +1,35 @@
+public class Usuario {
+    private String nombre;
+    private String id;
+    private String email;
+
+    public Usuario(String nombre, String id, String email) {
+        this.nombre = nombre;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
