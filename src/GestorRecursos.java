059a5710c6b4522426fec4c11281ebccc322b561
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-04-21 22:29:53 -0300

    feat: Implementar clase base GestorRecursos

---
+++ src/GestorRecursos.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
+
+
+public class GestorRecursos {
+    private Map<String, RecursoDigital> recursos;
+
+    public GestorRecursos() {
+        this.recursos = new HashMap<>();
+    }
+
+    public void addNewResource(RecursoDigital recursoDigital) {
+        String id = recursoDigital.getIdentificador();
+        recursos.put(id, recursoDigital);
+    }
+
+    public RecursoDigital searchResource(String id) {
+        RecursoDigital recursoDigital = recursos.get(id);
+        return recursoDigital;
+    }
+
+    public Collection<RecursoDigital> listAllResources() {
+        return recursos.values();
+    }
+}
