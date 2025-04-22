04c64bb659509d1fc7570cbaf0162272247967fc
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-04-22 20:27:05 -0300

    feat: implementar interfaces en libro y audiolibro

--- src/Audiolibro.java
+++ src/Audiolibro.java
@@ -1,5 +1,21 @@
-public class Audiolibro extends RecursoBase {
+import java.time.LocalDateTime;
+
+public class Audiolibro extends RecursoBase implements Prestable {
     public Audiolibro(String id, String titulo, EstadoRecurso estado) {
         super(id, titulo, estado);
+    }
+
+    @Override
+    public boolean estaDisponible() {
+        return false;
+    }
+
+    @Override
+    public LocalDateTime getFechaDevolucion() {
+        return null;
+    }
+
+    @Override
+    public void prestar() {
     }
 }
